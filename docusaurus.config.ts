11c284319e69febb1bd6fc6542cc7a317054fa14
nknakk2 <144705354+nknakk2@users.noreply.github.com>
2025-01-18 07:17:35 +0000

    hello

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
     // TODO( github project link )
     url: 'https://nknakk2.github.io',
     // TODO (github project name e.g. /simple-web-programming/ )
-    baseUrl: '/mine-platypusloth/',
+    baseUrl: '/platypusloth/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
